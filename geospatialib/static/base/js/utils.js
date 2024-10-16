f9c2fd3ecfd9f2f7ca94849397421b1839337bb6
Tamsyn Malabanan <tamsyn.malabanan@gmail.com>
2024-10-17 01:23:28 +0530

    Config HTML base html template

--- geospatialib/static/base/js/utils.js
+++ geospatialib/static/base/js/utils.js
@@ -1,17 +1,47 @@
-const toggleDarkMode = (event) => {
+const getStoredTheme = () => localStorage.getItem('theme')
+    
+const setStoredTheme = theme => localStorage.setItem('theme', theme)
+
+const getPreferredTheme = () => {
+    const storedTheme = getStoredTheme()
+    if (storedTheme) {
+      return storedTheme
+    }
+    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+}
+
+const setTheme = theme => {
+    if (theme === 'auto') {
+        theme = (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light')
+    }
+    
+    document.documentElement.setAttribute('data-bs-theme', theme)
+    setStoredTheme(theme)
+
     const toggles = document.querySelectorAll(`[onclick='toggleDarkMode(event)']`)
-    const currentTheme = document.documentElement.getAttribute('data-bs-theme')
-    if (currentTheme === 'light') {
-        document.documentElement.setAttribute('data-bs-theme', 'dark')
+    if (theme === 'light') {
         toggles.forEach(toggle => {
             toggle.classList.remove('bi-moon-fill')
             toggle.classList.add('bi-moon')
         })
     } else {
-        document.documentElement.setAttribute('data-bs-theme', 'light')
         toggles.forEach(toggle => {
             toggle.classList.remove('bi-moon')
             toggle.classList.add('bi-moon-fill')
         })
     }
+
 }
+
+const toggleDarkMode = (event) => {
+    const currentTheme = document.documentElement.getAttribute('data-bs-theme')
+    if (currentTheme === 'light') {
+        setTheme('dark')
+    } else {
+        setTheme('light')
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    setTheme(getPreferredTheme())
+})
