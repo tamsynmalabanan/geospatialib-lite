04cfd4b29ce47b48ac5d1a9b00632b74965d7f87
Tamsyn Malabanan <tamsyn.malabanan@gmail.com>
2024-10-22 08:14:56 +0530

    updated dataset model

--- geospatialib/apps/library/models.py
+++ geospatialib/apps/library/models.py
@@ -1,5 +1,8 @@
 from django.contrib.gis.db import models
+from django.utils.text import slugify
+from django.db.models import Q
 
+import shortuuid
 from urllib.parse import urlparse
 
 from . import choices
@@ -7,13 +10,27 @@ from . import choices
 from ..utils.general import form_helpers
 
 class MetaAbstractModel(models.Model):
-    added_by = models.ForeignKey("main.User", verbose_name='Added by', on_delete=models.DO_NOTHING, related_name='%(class)ss_added')
+    uuid = models.SlugField('UUID', unique=True, editable=False, null=True, blank=True, max_length=16)
+    added_by = models.ForeignKey("main.User", verbose_name='Added by', editable=False, on_delete=models.DO_NOTHING, related_name='%(class)ss_added')
+    updated_by = models.ForeignKey("main.User", verbose_name='Updated by', editable=False, on_delete=models.DO_NOTHING, blank=True, null=True, related_name='%(class)ss_updated')
     added_on = models.DateTimeField('Added on', auto_now_add=True)
-    updated_by = models.ForeignKey("main.User", verbose_name='Updated by', on_delete=models.DO_NOTHING, blank=True, null=True, related_name='%(class)ss_updated')
     updated_on = models.DateTimeField('Updated on', auto_now=True)
 
     class Meta:
         abstract = True
+
+    def assign_uuid(self):
+        if not self.uuid:
+            while True:
+                uuid = shortuuid.uuid()[:16]
+                if not self.__class__.objects.filter(uuid__iexact=uuid).exists():
+                    break
+            self.uuid = uuid
+
+    def save(self, *args, **kwargs):
+        self.assign_uuid()
+        super().save(*args, **kwargs)
+
 
 class URL(MetaAbstractModel):
     path = models.URLField('URL', max_length=256, unique=True)
